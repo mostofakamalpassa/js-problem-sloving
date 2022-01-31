aa14ce7bc2ee1e46f72ab962f380d14eed147559
mostofa kamal <mostofakamal1990@gmail.com>
2022-01-31 23:14:11 +0600

    swapping tow numbers

---
+++ swap.js
@@ -0,0 +1,21 @@
+
+
+var first = 10;
+var second = 20;
+
+
+console.log(first, second)
+
+let temp = first;
+
+first = second;
+
+second = temp;
+
+console.log(first, second);
+
+
+
+
+
+console.log(first, second);
