7577723dc9a7c82850fc214b80a6da5d09b4a8e1
mostofa kamal <mostofakamal1990@gmail.com>
2022-01-31 23:56:29 +0600

    smallest numbers

--- largest-element.js
+++ largest-element.js
@@ -14,5 +14,25 @@ function leargestElement(arrays){
 }
 
 
-console.log(leargestElement([5,6,7,100,50, 60]));
-console.log(leargestElement([-2,-44,-22,-10]));
+// console.log(leargestElement([5,6,7,100,50, 60]));
+// console.log(leargestElement([-2,-44,-22,-10]));
+
+
+function minimumNumbers(arrays){
+    let smallest = arrays[0];
+
+    for(let i = 0; i< arrays.length; i++){
+
+        if(arrays[i] < smallest){
+            smallest = arrays[i];
+        }
+    }
+
+    return smallest;
+
+}
+
+
+console.log(minimumNumbers([4,5,2,100, 1]));
+
+console.log(minimumNumbers([-1,22,-44,]));
