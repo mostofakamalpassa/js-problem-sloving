d7b6873bd350d3f77646e0d7a6d143a7769c2922
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-02 23:11:07 +0600

    centimet to meter Conversion

---
+++ centimeterToMeter.js
@@ -0,0 +1,10 @@
+function centimeterToMeter(centimeter){
+
+    return centimeter / 100;
+
+}
+
+
+console.log(centimeterToMeter(1000));
+console.log(centimeterToMeter(10));
+console.log(centimeterToMeter(500));
