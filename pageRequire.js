5c928f31dcb18a673d9c842bde039f75debb65f8
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-02 23:14:35 +0600

    page Require Of books

---
+++ pageRequire.js
@@ -0,0 +1,11 @@
+function pageRequires(numberBook, pageNumber){
+
+    return numberBook * pageNumber;
+}
+
+
+
+console.log(pageRequires(5, 100))
+console.log(pageRequires(3, 200))
+console.log(pageRequires(10, 150))
+console.log(pageRequires(8, 188))
