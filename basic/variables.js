0191153013fd344ae57dbb617673357b8630890c
mkpbd <mostofakamal1990@gmail.com>
2022-02-03 11:15:13 +0600

    Basic variable and simple Math operations

---
+++ basic/variables.js
@@ -0,0 +1,37 @@
+let name = "mosofa kamal";
+
+let isGrildFriend = true;
+let mobileNo = 01720246683;
+
+name = "mostofa kamal passa";
+
+const PI = 3.1214; // can not change 
+
+
+let firstNumber = 10;
+let secondNumber = 5;
+
+
+let addNumber = firstNumber + secondNumber;
+
+console.log("Addition Number = " , addNumber);
+
+
+let multipleyNumber = firstNumber * secondNumber;
+
+console.log("Multiplication Number = ", multipleyNumber);
+
+let substructionNumber = firstNumber - secondNumber;
+
+console.log("Substruction Number ",substructionNumber)
+
+let dividedNumber = firstNumber / secondNumber;
+
+console.log("Divided Nunbers = ", dividedNumber);
+
+
+let remiderNumber = firstNumber % secondNumber;
+
+console.log("Remider Numbers = ", remiderNumber)
+
+
