c2001da50b1a7367cea2e3b3faed2c92638d5edc
mkpbd <mostofakamal1990@gmail.com>
2022-02-03 12:02:21 +0600

    array for loop or for of loop uing display array data

---
+++ basic/forLoop.js
@@ -0,0 +1,18 @@
+
+
+let array  = ['mostofa','kamal','jamal','tomal', 'anorar',666,777,444];
+
+
+for(let i =0; i< array.length; i++){
+
+    console.log(array[i]);
+}
+
+
+console.log("For Of Loop ");
+
+
+for(let key of array){
+
+    console.log(key);
+}
