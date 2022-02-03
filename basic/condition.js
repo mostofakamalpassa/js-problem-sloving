7caff47cc34d95b0799e7a1aae6ad516095c2dde
mkpbd <mostofakamal1990@gmail.com>
2022-02-03 11:31:13 +0600

    condition cheacking

---
+++ basic/condition.js
@@ -0,0 +1,12 @@
+
+let  day = "friyDay";
+let isRaning = false;
+
+if(day == 'friyDay' && isRaning == true){
+    console.log( "i will slepping now");
+}
+
+
+if(day == 'friyDay' || isRaning== true){
+    console.log("I will go  market now")
+}
